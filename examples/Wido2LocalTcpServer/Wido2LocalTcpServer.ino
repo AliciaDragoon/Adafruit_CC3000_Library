9cecbf97602ff4347e341df5394ecacbaf0efcf5
Lauren-ED209 <lauren.pan@hotmail.com>
2014-08-01 04:12:18 +0800

    Failed to find a better local server
    Simplify the code and extend heart rate data transmission to the local
    server

--- examples/Wido2LocalTcpServer/Wido2LocalTcpServer.ino
+++ examples/Wido2LocalTcpServer/Wido2LocalTcpServer.ino
@@ -11,22 +11,16 @@
 Adafruit_CC3000 WiDo = Adafruit_CC3000(WiDo_CS, WiDo_IRQ, WiDo_VBAT,
                                          SPI_CLOCK_DIVIDER); // you can change this clock speed
 
-#define WLAN_SSID       "DFRobot WIFI"           // cannot be longer than 32 characters!
-#define WLAN_PASS       "DFRobot2014"
+#define WLAN_SSID       "pocketAP"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "Lauren714412"
 
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
-
-#define IDLE_TIMEOUT_MS  1000
+#define TIMEOUT_MS  1000
 
 void setup(){
   
   Serial.begin(115200);
-  Serial.println(F("Hello, CC3000!\n")); 
-
-  displayDriverMode();
-  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
-  
   /* Initialise the module */
   Serial.println(F("\nInitialising the CC3000 ..."));
   if (!WiDo.begin())
@@ -34,30 +28,7 @@ void setup(){
     Serial.println(F("Unable to initialise the CC3000! Check your wiring?"));
     while(1);
   }
-  
-  uint16_t firmware = checkFirmwareVersion();
-  if (firmware < 0x113) {
-    Serial.println(F("Wrong firmware version!"));
-    for(;;);
-  } 
-  
-//  displayMACAddress();
-  
-  /* Optional: Get the SSID list (not available in 'tiny' mode)
-  listSSIDResults();
-   */
-  /* Delete any old connection data on the module 
-  Serial.println(F("\nDeleting old connection profiles"));
-  if (!WiDo.deleteProfiles()) {
-    Serial.println(F("Failed!"));
-    while(1);
-  }
-  */
-  
-  /* Attempt to connect to an access point */
-  char *ssid = WLAN_SSID;             /* Max 32 chars */
-  Serial.print(F("\nAttempting to connect to ")); Serial.println(ssid);
-  
+    
   /* NOTE: Secure connections are not available in 'Tiny' mode!
      By default connectToAP will retry indefinitely, however you can pass an
      optional maximum number of retries (greater than zero) as the fourth parameter.
@@ -66,8 +37,8 @@ void setup(){
     Serial.println(F("Failed!"));
     while(1);
   }
-   
-  Serial.println(F("Connected!"));
+  
+  Serial.println(F("Router/AP Connected!"));
   
   /* Wait for DHCP to complete */
   Serial.println(F("Request DHCP"));
@@ -75,173 +46,46 @@ void setup(){
   {
     delay(100); // ToDo: Insert a DHCP timeout!
   }  
-
-  /* Display the IP address DNS, Gateway, etc. 
-  while (! displayConnectionDetails()) {
-    delay(1000);
-  }
-  */
 }
-
-uint16_t clientPushCounter = 0;
-uint32_t ip = 0;
 
 void loop(){
   
-  static Adafruit_CC3000_Client www;
-  if(!www.connected()){
-    Serial.println("Unable to connect the Local Server");
-    www.close();
-    ip = WiDo.IP2U32(192,168,0,134);
-    www = WiDo.connectTCP(ip, 4000);
-//    Serial.println("Connect TCP");
-  }
-  else{
-    www.fastrprint(F("Successed to connect local server!"));
-    www.fastrprint(F("\r\n"));
-  }
-
-//  Serial.println(F("-------------------------------------"));
+  static Adafruit_CC3000_Client tcpClient;
+  static unsigned long heartRate = millis();
   
-  /* Read data until either the connection is closed, or the idle timeout is reached. */ 
-  unsigned long lastRead = millis();
-  while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
-    while (www.available()) {
-      char c = www.read();
-      Serial.print(c);
-      lastRead = millis();
+  if(!tcpClient.connected()){
+    Serial.println("Try to connect the Local Server");
+    tcpClient.close();
+    
+    /* Set the target ip address and connection port */
+    uint32_t ip = WiDo.IP2U32(192,168,1,6);
+    tcpClient = WiDo.connectTCP(ip, 4500);
+    
+    if(!tcpClient.connected()){
+      Serial.println(F("Couldn't connect to server! Make sure TCP Test Tool is running on the server."));
+      while(1);
     }
   }
-//  delay(1000);
-}
-
-
-/**************************************************************************/
-/*!
-    @brief  Displays the driver mode (tiny of normal), and the buffer
-            size if tiny mode is not being used
-
-    @note   The buffer size and driver mode are defined in cc3000_common.h
-*/
-/**************************************************************************/
-void displayDriverMode(void)
-{
-  #ifdef CC3000_TINY_DRIVER
-    Serial.println(F("CC3000 is configure in 'Tiny' mode"));
-  #else
-    Serial.print(F("RX Buffer : "));
-    Serial.print(CC3000_RX_BUFFER_SIZE);
-    Serial.println(F(" bytes"));
-    Serial.print(F("TX Buffer : "));
-    Serial.print(CC3000_TX_BUFFER_SIZE);
-    Serial.println(F(" bytes"));
-  #endif
-}
-
-/**************************************************************************/
-/*!
-    @brief  Tries to read the CC3000's internal firmware patch ID
-*/
-/**************************************************************************/
-uint16_t checkFirmwareVersion(void)
-{
-  uint8_t major, minor;
-  uint16_t version;
-  
-#ifndef CC3000_TINY_DRIVER  
-  if(!WiDo.getFirmwareVersion(&major, &minor))
-  {
-    Serial.println(F("Unable to retrieve the firmware version!\r\n"));
-    version = 0;
-  }
-  else
-  {
-    Serial.print(F("Firmware V. : "));
-    Serial.print(major); Serial.print(F(".")); Serial.println(minor);
-    version = major; version <<= 8; version |= minor;
-  }
-#endif
-  return version;
-}
-
-
-/**************************************************************************/
-/*!
-    @brief  Tries to read the 6-byte MAC address of the CC3000 module
-*/
-/**************************************************************************/
-void displayMACAddress(void)
-{
-  uint8_t macAddress[6];
-  
-  if(!WiDo.getMacAddress(macAddress))
-  {
-    Serial.println(F("Unable to retrieve MAC Address!\r\n"));
-  }
-  else
-  {
-    Serial.print(F("MAC Address : "));
-    WiDo.printHex((byte*)&macAddress, 6);
-  }
-}
-
-
-/**************************************************************************/
-/*!
-    @brief  Tries to read the IP address and other connection details
-*/
-/**************************************************************************/
-bool displayConnectionDetails(void)
-{
-  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
-  
-  if(!WiDo.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
-  {
-    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
-    return false;
-  }
-  else
-  {
-    Serial.print(F("\nIP Addr: ")); WiDo.printIPdotsRev(ipAddress);
-    Serial.print(F("\nNetmask: ")); WiDo.printIPdotsRev(netmask);
-    Serial.print(F("\nGateway: ")); WiDo.printIPdotsRev(gateway);
-    Serial.print(F("\nDHCPsrv: ")); WiDo.printIPdotsRev(dhcpserv);
-    Serial.print(F("\nDNSserv: ")); WiDo.printIPdotsRev(dnsserv);
-    Serial.println();
-    return true;
-  }
-}
-
-/**************************************************************************/
-/*!
-    @brief  Begins an SSID scan and prints out all the visible networks
-*/
-/**************************************************************************/
-
-void listSSIDResults(void)
-{
-  uint8_t valid, rssi, sec, index;
-  char ssidname[33]; 
-
-  index = WiDo.startSSIDscan();
-
-  Serial.print(F("Networks found: ")); Serial.println(index);
-  Serial.println(F("================================================"));
-
-  while (index) {
-    index--;
-
-    valid = WiDo.getNextSSID(&rssi, &sec, ssidname);
+  else if(millis() - heartRate > 1000){
+    heartRate = millis();  // Update time stamp of the microcontroller system
     
-    Serial.print(F("SSID Name    : ")); Serial.print(ssidname);
-    Serial.println();
-    Serial.print(F("RSSI         : "));
-    Serial.println(rssi);
-    Serial.print(F("Security Mode: "));
-    Serial.println(sec);
-    Serial.println();
+    String clientString = "";
+    clientString = clientString + "Wido Time: " + heartRate/1000 + " s";
+    
+    tcpClient.println(clientString);
+    Serial.println(clientString);
   }
-  Serial.println(F("================================================"));
-
-  WiDo.stopSSIDscan();
+  
+  /* Read data until either the connection is closed, or the timeout is reached. */ 
+  unsigned long lastRead = millis();
+  while (tcpClient.connected() && (millis() - lastRead < TIMEOUT_MS)) {
+    while (tcpClient.available()) {
+      char c = tcpClient.read();
+      Serial.print(c);
+      lastRead = millis();
+      
+      // Disable sending message for a moment
+      heartRate = millis();
+    }
+  }
 }
