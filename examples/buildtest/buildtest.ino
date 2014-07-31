30c560b073635a9076eaad3b266db4b3a049b12c
Lauren <lauren.pan@hotmail.com>
2014-07-31 21:59:24 +0800

    Move and modify buildtest to main sample

--- examples/buildtest/buildtest.ino
+++ examples/buildtest/buildtest.ino
@@ -13,7 +13,7 @@
  ****************************************************/
 
 /*
-
+Modified the pin configuration for Wido
 This example does a full test of core connectivity:
 * Initialization
 * SSID Scan
@@ -33,36 +33,35 @@ module.
 #include <string.h>
 #include "utility/debug.h"
 
-// These are the interrupt and control pins
-#define ADAFRUIT_CC3000_IRQ   7  // MUST be an interrupt pin!
-// These can be any two pins
+// Config the interrupt and control pins on Wido
+#define ADAFRUIT_CC3000_IRQ   7
 #define ADAFRUIT_CC3000_VBAT  5
 #define ADAFRUIT_CC3000_CS    10
-// Use hardware SPI for the remaining pins
-// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
                                          SPI_CLOCK_DIVIDER); // you can change this clock speed but DI
 
+//Please enter the SSID and password of the router you want to connect
+
 #define WLAN_SSID       "myNetwork"        // cannot be longer than 32 characters!
 #define WLAN_PASS       "myPassword"
+
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
 
-
-/**************************************************************************/
-/*!
-    @brief  Sets up the HW and the CC3000 module (called automatically
-            on startup)
-*/
-/**************************************************************************/
 void setup(void)
 {
   Serial.begin(115200);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
   Serial.println(F("Hello, CC3000!\n")); 
-
+  
+  
   displayDriverMode();
-  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
+  //Measure the free Ram
+  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC); 
   
   /* Initialise the module */
   Serial.println(F("\nInitialising the CC3000 ..."));
